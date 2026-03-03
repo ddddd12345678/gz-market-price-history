285314df7868cca08364dba0b6dc89e0018f3839
dddd <57289086+ddddd12345678@users.noreply.github.com>
2026-03-03 18:00:18 +0800

    2026-03-03 data

--- jsonp-data/by-month/2026-03/粳米-2026-03.js
+++ jsonp-data/by-month/2026-03/粳米-2026-03.js
@@ -1,1 +1,1 @@
-jsonp_data_by_month_2026_03_粳米_2026_03_js({"wholeCity": [{"date": "2026-03-01", "price": "6.22"}, {"date": "2026-03-02", "price": "6.22"}]})
+jsonp_data_by_month_2026_03_粳米_2026_03_js({"wholeCity": [{"date": "2026-03-01", "price": "6.22"}, {"date": "2026-03-02", "price": "6.22"}, {"date": "2026-03-03", "price": "6.22"}]})
--- jsonp-data/by-month/2026-03/花生米-2026-03.js
+++ jsonp-data/by-month/2026-03/花生米-2026-03.js
@@ -1,1 +1,1 @@
-jsonp_data_by_month_2026_03_花生米_2026_03_js({"wholeCity": [{"date": "2026-03-01", "price": "15.79"}, {"date": "2026-03-02", "price": "15.79"}]})
+jsonp_data_by_month_2026_03_花生米_2026_03_js({"wholeCity": [{"date": "2026-03-01", "price": "15.79"}, {"date": "2026-03-02", "price": "15.79"}, {"date": "2026-03-03", "price": "15.79"}]})
