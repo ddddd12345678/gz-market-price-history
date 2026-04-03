82864ccbf37e64143ad2853856504e68055657e7
dddd <57289086+ddddd12345678@users.noreply.github.com>
2026-04-03 18:00:17 +0800

    2026-04-03 data

--- jsonp-data/by-month/2026-04/香蕉-2026-04.js
+++ jsonp-data/by-month/2026-04/香蕉-2026-04.js
@@ -1,1 +1,1 @@
-jsonp_data_by_month_2026_04_香蕉_2026_04_js({"wholeCity": [{"date": "2026-04-01", "price": "6.40"}, {"date": "2026-04-02", "price": "6.40"}]})
+jsonp_data_by_month_2026_04_香蕉_2026_04_js({"wholeCity": [{"date": "2026-04-01", "price": "6.40"}, {"date": "2026-04-02", "price": "6.40"}, {"date": "2026-04-03", "price": "6.44"}]})
